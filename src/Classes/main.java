ba17695511e846ec84634d2e700fe3bfeb6620db
Pratik Talele <pratiktalele22@gmail.com>
2021-07-30 15:37:26 +0530

    commiting uc1

---
+++ src/Classes/main.java
@@ -0,0 +1,11 @@
+package Classes;
+
+public class main {
+	public static void main(String[] args) {
+		//creating object for uc1
+		UC1 moodAnalyser = new UC1();
+		System.out.println(moodAnalyser.analyseMood("sad"));
+		System.out.println(moodAnalyser.analyseMood("happy"));
+
+	}
+}
